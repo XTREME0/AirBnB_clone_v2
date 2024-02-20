d01e9604c82e6fc126bcfb8e9705d51bea218abb
ayoub <sb.naba2@gmail.com>
2024-02-20 20:27:02 +0100

    some modification in do_create function

--- console.py
+++ console.py
@@ -128,7 +128,7 @@ class HBNBCommand(cmd.Cmd):
             val_key[1] = val_key[1].replace("_", " ").replace('"', '\\"')
             kwargs[val_key[0]] = val_key[1]
         new_instance = HBNBCommand.classes[args_list[0]](**kwargs)
-        new_instance.save()
+        storage.save()
         print(new_instance.id)
         storage.save()
 
