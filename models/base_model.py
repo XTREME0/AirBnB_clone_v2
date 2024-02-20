0e18c1f7d55154df0154edd0edb67d763bfff0e4
ayoub <sb.naba2@gmail.com>
2024-02-20 23:17:59 +0100

    some changes on Basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,15 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(
+                                value, "%Y-%m-%dT%H:%M:%S.%f"
+                                )
+                    setattr(self, key, value)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
             self.__dict__.update(kwargs)
 
     def __str__(self):
