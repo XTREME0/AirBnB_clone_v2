6a54a235ec0d0941e31bfebcbb9eea869fcf473e
xtreme <anastaai94@gmail.com>
2024-03-23 18:15:33 +0000

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+run a simple flask web app
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbtn():
+    """
+    print hello hbtn
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    print hbnb
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """
+    print text
+    """
+    x = text.replace('_', ' ')
+    return "C " + x
+
+
+@app.route("/python", strict_slashes=False)
+def py():
+    """
+    print text
+    """
+    return "Python is cool"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def pytxt(text="is cool"):
+    """
+    print text
+    """
+    x = text.replace('_', ' ')
+    return "Python " + x
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
